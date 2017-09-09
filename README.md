b07037599b83409ad4a68c49ea8f38d3a404282d
Dzmitry Vashkevich <dimalepel28443@gmail.com>
2017-09-09 22:38:43 +0300

    адаптив

--- README.md
+++ README.md
@@ -1,34 +1,34 @@
 # trals
 Верстка Тралс
 
-Главная https://dimalepel.github.io/trals/
+Главная https://dimalepel.github.io/trals/ д п м
 
-Поп-ап карта https://dimalepel.github.io/trals/ya-map.html
+Поп-ап карта https://dimalepel.github.io/trals/ya-map.html д п м
 
-Карта сайта https://dimalepel.github.io/trals/site-map.html
+Карта сайта https://dimalepel.github.io/trals/site-map.html д п м
 
-Карточка товара https://dimalepel.github.io/trals/product.html
+Карточка товара https://dimalepel.github.io/trals/product.html д п м
 
-Каталог https://dimalepel.github.io/trals/catalog.html
+Каталог https://dimalepel.github.io/trals/catalog.html д п м
 
-Контакты https://dimalepel.github.io/trals/contacts.html
+Контакты https://dimalepel.github.io/trals/contacts.html д п м
 
-О компании https://dimalepel.github.io/trals/about.html
+О компании https://dimalepel.github.io/trals/about.html д п м
 
-Партнеры https://dimalepel.github.io/trals/partners.html
+Партнеры https://dimalepel.github.io/trals/partners.html д п м
 
-Результат поиска(ничего не найдено) https://dimalepel.github.io/trals/search-no-result.html
+Результат поиска(ничего не найдено) https://dimalepel.github.io/trals/search-no-result.html д п м
 
-Результат поиска https://dimalepel.github.io/trals/search-result.html
+Результат поиска https://dimalepel.github.io/trals/search-result.html д п м
 
-Статьи(Побробнее) https://dimalepel.github.io/trals/article-detail.html
+Статьи(Побробнее) https://dimalepel.github.io/trals/article-detail.html д п м
 
-Статьи https://dimalepel.github.io/trals/articles.html
+Статьи https://dimalepel.github.io/trals/articles.html д п м
 
-Страница 404 https://dimalepel.github.io/trals/404.html
+Страница 404 https://dimalepel.github.io/trals/404.html д п м
 
-Торговые марки(Побробнее) https://dimalepel.github.io/trals/brand-detail.html
+Торговые марки(Побробнее) https://dimalepel.github.io/trals/brand-detail.html д п м
 
-Торговые марки https://dimalepel.github.io/trals/brands.html
+Торговые марки https://dimalepel.github.io/trals/brands.html д п м
 
-Страница со списком https://dimalepel.github.io/trals/page.html
+Страница со списком https://dimalepel.github.io/trals/page.html д п м
--- js/common.js
+++ js/common.js
@@ -7,8 +7,19 @@ $('.brands-slider').owlCarousel({
 	smartSpeed:1750, // плавность смены слайда
   margin:0,
   nav:true,
-  items:4
+	responsive : {
+    0 : {
+      items:1
+    },
+    768 : {
+       items:3
+    },
+    1200 : {
+      items:4
+    }
+	}
 });
+
 //Кнопка "Наверх"
 $('#back_top').click(function () {
 	$('body,html').animate({
@@ -16,8 +27,18 @@ $('#back_top').click(function () {
 	}, 600);
 	return false;
 });
+
 // Модальные окна
 $(".js-open-modal").fancybox({
+	wrapCSS: 'fb-modal-win',
+	padding : 0,
+	helpers: {
+		overlay: {
+			locked: false
+		}
+	}
+});
+$(".js-img-modal").fancybox({
 	wrapCSS: 'fb-modal-win',
 	padding : 30,
 	helpers: {
@@ -26,15 +47,7 @@ $(".js-open-modal").fancybox({
 		}
 	}
 });
-// Галерея карточки товара
-$(".photosgallery-vertical").sliderkit({
-	circular:true,
-	mousewheel:true,
-	shownavitems:4,
-	verticalnav:true,
-	navclipcenter:true,
-	auto:false
-});
+
 // Ползунки параметров
 function SliderMinMax(selector,min,max,step,range){
 	$(selector).slider({
@@ -54,55 +67,100 @@ function SliderMinMax(selector,min,max,step,range){
 	});
 };
 SliderMinMax('.factory',0.75,4,0.05,true);
+
 // Спойлер для списков фильтра
-$('ul.checkbox').each(function(){
-	$(this).find('li:gt(9)').hide();
-	if ($(this).find('li').length > 10) {
-		$(this).append('<div class = "btn gray see-more" title = "">Показать еще</div>');
-	}
-})
-$('div.see-more').click(function(){
-	var ul = $(this).parent();
-	// ul.find('li:hidden:lt(99)').show();
-	ul.find('li:gt(9)').slideToggle();
-	ul.find('li:hidden:lt(99)').slideToggle();
-	if (ul.find('li.hidden').length == 0) {
-		// $(this).html('Скрыть');
-		// $(this).hide();
-	}
-})
-let button = document.querySelector("div.see-more");
-let shown = false;
-button.addEventListener("click", event => {
-  if (shown) {
-  	button.textContent = "Показать еще";
-  } else {
-  	button.textContent = "Скрыть";
-  }
-  
-  shown = shown ? false : true;
-}, false);
+var maxLi = 10, text = ["Скрыть", "Показать еще"];
+$("ul.checkbox").each(function() {
+    var li = $(this).find("li");
+    if (li.length > maxLi) {
+        li = li.slice(maxLi).hide();
+        var btn = $("<div>", {
+            text: text[1],
+            "class": "btn gray see-more",
+            click: function() {
+                li.stop().slideToggle(500, function() {
+                    btn.text(text[+$(this).is(":hidden")])
+                })
+            }
+        }).appendTo(this)
+    }
+});
+
 // Аккордион
 $('.accordion-item').click(function(){
 	$(this).siblings('.data').slideToggle();
 	$(this).toggleClass('close');
 })
-/*$(function($) {
-  var allAccordions = $('.accordion div.data');
-  var allAccordionItems = $('.accordion .accordion-item');
-  $('.accordion .aside__filter__item > .accordion-item').click(function() {
-    if($(this).hasClass('open'))
-    {
-      $(this).removeClass('open');
-      $(this).next().slideUp("slow");
-    }
-    else
-    {
-    allAccordions.slideUp("slow");
-    allAccordionItems.removeClass('open');
-    $(this).addClass('open');
-    $(this).next().slideDown("slow");
-    return false;
-    }
-  });
-});*/
+
+// Вызов меню слева
+$(".mobile-trigger").click(function() {
+	$(".bd-site").toggleClass('mob-nav-open');
+	$("body").toggleClass('overflow');
+});
+$(".mobile-aside__nav__close").click(function() {
+	$(".bd-site").toggleClass('mob-nav-open');
+	$("body").toggleClass('overflow');
+});
+
+/* Контакты справа */
+$(".mobile-address-trigger").click(function() {
+	$(".bd-site").toggleClass('mob-contact-open');
+	$("body").toggleClass('overflow');
+});
+$(".mobile-aside__contact__close").click(function() {
+	$(".bd-site").toggleClass('mob-contact-open');
+	$("body").toggleClass('overflow');
+});
+
+// Выпадающее мобильное меню
+$(".has-children a").click(function() {
+	$(".has-children ul").slideToggle();
+});
+
+// Вызов мобильного фильтра
+$(".mobile-filter-trigger").click(function() {
+	$(".bd-site").toggleClass('mob-filter-open');
+	$("body").toggleClass('overflow');
+});
+$(".aside__filter__close").click(function() {
+	$(".bd-site").toggleClass('mob-filter-open');
+	$("body").toggleClass('overflow');
+});
+
+// Обрезаем текст для превью статей планшета
+function title() {  
+  var elem, size, text;
+  elem = document.getElementsByClassName('prew');
+  text = elem.innerHTML;
+  size = 180; 
+  for(var i = 0; i < elem.length; i++) { 
+    if(elem[i].innerHTML.length > size) {
+      text = elem[i].innerHTML.substr(0, size);
+    }    
+    elem[i].innerHTML = text + '...';
+  }
+}
+if(document.documentElement.clientWidth < 1200) {
+	title();
+}
+
+// Галерея карточки товара
+if(document.documentElement.clientWidth < 768) {
+	$(".photosgallery-vertical").sliderkit({
+		circular:true,
+		mousewheel:true,
+		shownavitems:99,
+		verticalnav:true,
+		navclipcenter:true,
+		auto:false
+	});
+} else {
+	$(".photosgallery-vertical").sliderkit({
+		circular:true,
+		mousewheel:true,
+		shownavitems:4,
+		verticalnav:true,
+		navclipcenter:true,
+		auto:false
+	});
+}
